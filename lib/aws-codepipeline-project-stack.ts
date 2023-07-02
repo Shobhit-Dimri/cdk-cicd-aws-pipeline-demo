fd58e74dcc988b833d20096bf1c3f38ddb2ca1f9
Shobhit Dimri <dimrishobhit@gmail.com>
2023-07-02 15:02:48 +0530

    Setting up the cdk code pipeline project.

--- lib/aws-codepipeline-project-stack.ts
+++ lib/aws-codepipeline-project-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCodepipelineProjectStack extends cdk.Stack {
@@ -12,5 +14,17 @@ export class AwsCodepipelineProjectStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'AwsCodepipelineProjectQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'CDKTestPipeline',       // Creating a new code pipeline which is a construct
+      synth: new ShellStep('Synth', {        // Add a new synthesis 'shellstep' which will be pointed at our gihub repository 
+        input: CodePipelineSource.gitHub('aLLUPS/aws-codepipeline-project', 'main'), // replace the GitHub repository name with 'user-name/repository-name'
+        
+        // The build steps for the pipeline are defined by these commands
+        
+        commands: ['npm ci',
+                   'npm run build',
+                   'npx cdk synth']
+      }),
+    })
   }
 }
